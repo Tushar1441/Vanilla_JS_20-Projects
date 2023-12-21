80013329321a3d2193b6e8c2affdcd3194176190
Tushar Bansal <tusharbansal069@gmail.com>
2023-12-21 17:53:46 +0530

    updated project-04

--- Project 4 - To Do List/script.js
+++ Project 4 - To Do List/script.js
@@ -2,7 +2,8 @@ const push = document.getElementById("push-btn");
 const tasks = document.getElementById("tasks");
 const newtask = document.querySelector("#newtask input");
 
-push.onclick = function () {
+// Function to add a new task
+function addNewTask() {
   if (newtask.value.length === 0) {
     alert("Please Enter a task");
   } else {
@@ -17,25 +18,37 @@ push.onclick = function () {
             </button>
         </div>
     `;
-    
-    // all the delete buttons will be stored in current_tasks
-    var current_tasks = document.querySelectorAll(".delete-btn");
-    for (var i = 0; i < current_tasks.length; i++) {
-      current_tasks[i].onclick = function () {
-        this.parentNode.remove();
-      };
-    }
-
-    // all tasks will be stored inside the all_tasks
-    var all_tasks = document.querySelectorAll(".task");
-    // crossing a completed task
-    for (var i = 0; i < all_tasks.length; i++) {
-      all_tasks[i].onclick = function () {
-        this.classList.toggle("completed");
-      };
-    }
+    // Set up event listeners for delete buttons and task completion
+    setEventListeners();
 
     // clearing the input field
     newtask.value = " ";
   }
-};
+}
+
+// Function to set up event listeners for delete buttons and task completion
+function setEventListeners() {
+  var current_tasks = document.querySelectorAll(".delete-btn");
+  for (var i = 0; i < current_tasks.length; i++) {
+    current_tasks[i].onclick = function () {
+      this.parentNode.remove();
+    };
+  }
+
+  var all_tasks = document.querySelectorAll(".task");
+  for (var i = 0; i < all_tasks.length; i++) {
+    all_tasks[i].onclick = function () {
+      this.classList.toggle("completed");
+    };
+  }
+}
+
+// Event listener for the "push" button
+push.onclick = addNewTask;
+
+// Event listener for the input field to add a task on "Enter" key press
+newtask.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addNewTask();
+  }
+});
